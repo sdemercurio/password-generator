ce057562fdfcb73e57dfc638c7f64e148ea95827
Sarah DeMercurio <sdemercuriomi@gmail.com>
2020-12-13 15:35:21 -0500

    fixing code in script to generate password.

--- assets/script.js
+++ assets/script.js
@@ -5,49 +5,46 @@ function generatePassword() {
 
 // crear array de cada tipo de los caracteres.
 
-var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+var charArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 
 // empty array
  
 var resultArray = [];
 var userArray = [];
 
-uppercaseArray [1]
+upperArray [1]
 //-----------------------------------------------------------------
-  var numCharacter = prompt ("How much number of Character you want between 8 and 128?");
-  var numbers = confirm ("you want numbers in your password?");
+  var passLength = prompt ("How much number of Character you want between 8 and 128?");
   var uppercases = confirm ("you want Uppercases in your password?");
   var lowercases = confirm ("you want lowercases in your password?");
+  var numbers = confirm ("you want numbers in your password?");
   var characters = confirm ("you want special characters in your password?");
 
 
 // condition of the array
 
+if (uppercases){
+    resultArray = resultArray.concat(upperArray);
+}
+  
+if (lowercases){
+    resultArray = resultArray.concat(lowerArray);
+}
+
 if (numbers){
   resultArray = resultArray.concat(numbArray);
-  
-}
-
-if (uppercases){
-  resultArray = resultArray.concat(uppercaseArray);
-
-}
-
-if (lowercases){
-  resultArray = resultArray.concat(lowercaseArray);
-
 }
 
 if (characters){
-  resultArray = resultArray.concat(characterArray);
+  resultArray = resultArray.concat(charArray);
 }
 console.log(resultArray)
 
 
-for (var i = 0; i < numCharacter; i++) {
+for (var i = 0; i < passLength; i++) {
       
   userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
   }
