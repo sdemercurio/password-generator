9d4a3bc2be4b5b2349bb7603ba68770946c51a97
Sarah DeMercurio <sdemercuriomi@gmail.com>
2020-12-13 06:41:42 -0500

    adding potential functions to script.js

--- assets/script.js
+++ assets/script.js
@@ -27,14 +27,52 @@ THEN a password is generated that matches the selected criteria */
 /* WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page */
 
-function begin() {
-    var message;
-    var characterLength = prompt("Password must be between 8 and 128 characters. Enter desired length of password.");
-    if (characterLength >= 8) {
-        prompt("Do you want lowercase characters?");
-    } else {
-        prompt("Please enter a value between 8 and 128.");
-    } console.log(characterLength);
+//function begin() {
+ //   var message;
+ //   var characterLength = prompt("Password must be between 8 and 128 characters. Enter desired length of password.");
+//    if (characterLength >= 8) {
+ //       prompt("Do you want lowercase characters?");
+ //   } else {
+ //       prompt("Please enter a value between 8 and 128.");
+ //   } console.log(characterLength);
+//}
+
+// DOM elements
+
+
+const randomFunctions = {
+    lowercase: getLowercase,
+    uppercase: getUppercase,
+    number: getRandomNumber,
+    specialCharacter: getSpecialChar,
 }
+
+/* Functions for generating random letters, numbers, and characters referencing 
+a character set.
+http://www.net-comber.com/charset.html */
+
+// To generate random lowercase letters. 
+
+function getLowercase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+// To generate random uppercase letters. 
+function getUppercase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+// To generate random numbers. 
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+// To generate random special characters. 
+function getSpecialChar() {
+    const symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getSpecialChar());
 
 
