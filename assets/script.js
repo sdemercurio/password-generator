806096dddd7110f2e6b91f31ff238651c760c6ea
Sarah DeMercurio <sdemercuriomi@gmail.com>
2020-12-15 12:43:15 -0500

    cleaning up html and css

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,53 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-// crear array de cada tipo de los caracteres.
+// Arrays for each character type.
 
-var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var charArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 
-// empty array
+// Empty array
  
 var resultArray = [];
 var userArray = [];
 
-upperArray [1]
-//-----------------------------------------------------------------
-  var passLength = prompt ("How much number of Character you want between 8 and 128?");
+uppercaseArray [1]
+
+  var numCharacter = prompt ("How much number of Character you want between 8 and 128?");
+  var numbers = confirm ("you want numbers in your password?");
   var uppercases = confirm ("you want Uppercases in your password?");
   var lowercases = confirm ("you want lowercases in your password?");
-  var numbers = confirm ("you want numbers in your password?");
   var characters = confirm ("you want special characters in your password?");
 
 
-// condition of the array
-
-if (uppercases){
-    resultArray = resultArray.concat(upperArray);
-}
-  
-if (lowercases){
-    resultArray = resultArray.concat(lowerArray);
-}
+// Conditionals
 
 if (numbers){
   resultArray = resultArray.concat(numbArray);
+  
+}
+
+if (uppercases){
+  resultArray = resultArray.concat(uppercaseArray);
+
+}
+
+if (lowercases){
+  resultArray = resultArray.concat(lowercaseArray);
+
 }
 
 if (characters){
-  resultArray = resultArray.concat(charArray);
+  resultArray = resultArray.concat(characterArray);
 }
 console.log(resultArray)
 
 
-for (var i = 0; i < passLength; i++) {
+for (var i = 0; i < numCharacter; i++) {
       
   userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
   }
@@ -58,16 +61,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  // copyBtn.removeAttribute("disabled");
-  // copyBtn.focus();
 }
 
-function copyToClipboard() {
-  // BONUS 
-}
 
-// Add event listener to generate button
+// Event listener for generate button
 generateBtn.addEventListener("click", writePassword);
-
-// BONUS EVENT LISTENER
